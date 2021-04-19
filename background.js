9219be45573b0a5eaeb82cb6d06fcbad77957e38
tomtiao <47553271+tomtiao@users.noreply.github.com>
2021-04-19 16:27:43 +0800

    revert changes to login process. add dark theme

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
-let username = '123456789',
-    password = '123456',
+let default_username = '123456789',
+    default_password = '123456',
     autoClose = true;
 
 chrome.runtime.onInstalled.addListener(object => {
     if (object.reason == 'install') {
-        chrome.storage.sync.set({ username, password, autoClose });
-        console.log(`Default username and password set to ${username} and ${password}`);
+        chrome.storage.sync.set({ username: default_username, password: default_password, autoClose });
+        console.log(`Default username and password set to ${default_username} and ${default_password}`);
         console.log(`Automatically close the tab after login is set to ${autoClose}`);
         chrome.runtime.openOptionsPage(() => {
             console.log(`First time launch, opening options.`);
@@ -13,9 +13,9 @@ chrome.runtime.onInstalled.addListener(object => {
     }
 });
 
-// close the tab after login
+// message from content.js, close the tab after login
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.closeTab) {
         chrome.tabs.remove(sender.tab.id);
     }
-})
+});
--- content.js
+++ content.js
@@ -1,56 +1,48 @@
 const app = {
     start: () => {
-        if (self !== undefined) {
-            // browser
-            let work;
-            if (self.location.href.includes('logout.html')) {
-                // close the tab after logon if user had chosen to
-                work = function () {
-                    chrome.storage.sync.get(['autoClose'], ({ autoClose }) => {
-                        if (autoClose) {
-                            chrome.runtime.sendMessage({ closeTab: true });
-                        }
-                    });
-                };
-            } else {
-                // needs login
-                work = function () {
-                    let loginUsername;
-                    let loginPassword;
-
-                    chrome.storage.sync.get(['username', 'password'], ({ username, password }) => {
-                        loginUsername = username;
-                        loginPassword = password;
-
-                        if (loginUsername && loginPassword) {
-                            const username_input = document.querySelector(`input[id='userid']`);
-                            const password_input = document.querySelector(`input[id='passwd']`);
-    
-                            username_input.value = loginUsername;
-                            password_input.value = loginPassword;
-    
-                            window.checkSubmit(); // available in portalUtil.js, execute this will send the login request
-    
-                        } else {
-                            alert(`${chrome.i18n.getMessage('extInvalidInputAlert')}
-                            ${loginUsername} ${loginPassword}`);
-                            chrome.runtime.openOptionsPage();
-                        }
-                    });
-                };
-            }
-
-            if (document.readyState == 'loading') {
-                document.head.addEventListener('load', e => {
-                    if (e.target.tagName === 'SCRIPT'
-                    && e.target.endsWith('portalUtil.js')) {
-                        work();
+        let work;
+        if (window.location.href.includes('logout.html')) {
+            // close the tab after logon if user had chosen to
+            work = function () {
+                chrome.storage.sync.get(['autoClose'], ({ autoClose }) => {
+                    if (autoClose) {
+                        chrome.runtime.sendMessage({ closeTab: true });
                     }
                 });
-            } else {
-                work();
-            }
+            };
+        } else {
+            // needs login
+            work = function () {
+                let loginUsername;
+                let loginPassword;
+
+                chrome.storage.sync.get(['username', 'password'], ({ username, password }) => {
+                    loginUsername = username;
+                    loginPassword = password;
+                    
+                    const messages = {
+                        INVALID_INPUT: `${chrome.i18n.getMessage('extInvalidInputAlert')}
+                        ${loginUsername} ${loginPassword}`,
+                    };
+
+                    if (loginUsername && loginPassword) {
+                        const username_input = document.querySelector(`input[id='userid']`);
+                        const password_input = document.querySelector(`input[id='passwd']`);
+
+                        username_input.value = loginUsername;
+                        password_input.value = loginPassword;
+
+                        const login_btn = document.querySelector(`button[id='loginsubmit']`);
+
+                        login_btn.click(); // do this terrible thing for now
+                    } else {
+                        alert(messages.INVALID_INPUT);
+                        chrome.runtime.openOptionsPage();
+                    }
+                });
+            };
         }
+        work();
     }
 };
 app.start();
