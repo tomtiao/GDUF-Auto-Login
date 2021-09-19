3b1268d0dd5876cd4d980b45816af265c4a4e8ac
tomtiao <47553271+tomtiao@users.noreply.github.com>
2021-09-19 16:34:43 +0800

    extract classes

---
+++ component.js
@@ -0,0 +1,39 @@
+export default class Component {
+    
+    constructor(/** @type {string} */ selectorOfTemplate) {
+        if (this.constructor === Component) throw new Error('cannot instantiate abstract class Component.');
+
+        const template = document.querySelector(selectorOfTemplate);
+        if (!(template instanceof HTMLTemplateElement)) throw new TypeError(`expect <template>, got ${template}.`);
+        
+        /** @type {HTMLTemplateElement} */ this.template = template;
+        /** @type {HTMLElement} */ this.instance = null;
+    }
+
+    /**
+     * initialize self asynchronously.
+     */
+    async init() { throw new Error('not implemented') }
+
+    /**
+     * append content to dest asynchronously.
+     * @param {HTMLElement} dest destination
+     */
+    async render(dest) {
+        await this.init();
+        // transfering the template content rather than
+        // copying it to preserve eventlisteners.
+        dest.append(document.adoptNode(this.template.content, true));
+    }
+
+    /**
+     * visually show self asynchronously.
+     */
+    async show() { this.instance.hidden = false; }
+
+    /**
+     * visually hide self asynchronously.
+     */
+    async hide() { this.instance.hidden = true; }
+
+}
---
+++ swapper.js
@@ -0,0 +1,35 @@
+export default class Swapper {
+
+    static EVENT_TYPE = 'swap';
+
+    constructor(/** @type {string} */ selectorOfList) {
+        const list = document.querySelector(selectorOfList);
+        if (!(list instanceof HTMLUListElement)) throw new TypeError(`expect <ul>, got ${list}`);
+        
+        /** @type {HTMLUListElement} */ this.list = list;
+
+        this.list.querySelectorAll('a').forEach(link =>
+            link.textContent =
+                link.dataset.message ?
+                chrome.i18n.getMessage(link.dataset.message) :
+                '');
+
+        this.list.addEventListener('click', e => {
+            if (e.target instanceof HTMLAnchorElement && e.target.dataset.type) {
+                e.preventDefault();
+
+                this.list.querySelectorAll('a[data-type]')
+                    .forEach(ele => ele.classList.remove('current'));
+                e.target.classList.add('current');
+                this.changeType(e.target.dataset.type);
+            }
+        });
+    }
+
+    changeType(/** @type {string} */ type) {
+        const swapEvt = new CustomEvent(Swapper.EVENT_TYPE, { bubbles: true, detail: type });
+
+        this.list.dispatchEvent(swapEvt);
+    }
+
+}
