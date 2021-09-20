8f3623b66a7b417a22b1c1133478e1014d708756
tomtiao <47553271+tomtiao@users.noreply.github.com>
2021-09-20 12:52:44 +0800

    refactor content.js

--- content.js
+++ content.js
@@ -9,19 +9,19 @@
 async function sendLoginRequest(username, password) {
     const REQUEST_URL = new URL(`/quickauth.do`, window.location.origin);
 
-    const pages_params = new URLSearchParams(window.location.search);
+    const pageParams = new URLSearchParams(window.location.search);
     const VERSION = 0;
     const PORTALPAGEID = 1;
 
     const params = {
         userid: username,
         passwd: password,
-        wlanuserip: pages_params.get('wlanuserip'),
-        wlanacname: pages_params.get('wlanacname'),
+        wlanuserip: pageParams.get('wlanuserip'),
+        wlanacname: pageParams.get('wlanacname'),
         wlanacIp: '',
         ssid: '',
-        vlan: pages_params.get('vlan'),
-        mac: encodeURIComponent(pages_params.get('mac')),
+        vlan: pageParams.get('vlan'),
+        mac: encodeURIComponent(pageParams.get('mac')),
         version: VERSION,
         portalpageid: PORTALPAGEID,
         timestamp: Date.now(),
@@ -57,58 +57,70 @@ function getNotice(type) {
     }
 }
 
-const app = {
-    start: async () => {
-        if (window.location.href.includes('logout.html')) { // logout page, logout.html
-            // close the tab after logon if user had chosen to
-            getNotice('logout');
-            chrome.storage.sync.get(['autoClose'], ({ autoClose }) => {
+/**
+ * get values from the browser storage
+ * @param {string[]} keys 
+ * @returns {Promise<Record<string, unknown>>} values
+ */
+const getValue = async (keys) => new Promise(resolve => chrome.storage.sync.get(keys, resolve));
+
+const start = async () => {
+    if (window.location.href.includes('logout.html')) { // logout page, logout.html
+        // close the tab after logon if user had chosen to
+        getNotice('logout');
+
+        getValue(['autoClose'])
+            .then(({ autoClose }) => {
                 if (autoClose) {
                     chrome.runtime.sendMessage({ closeTab: true });
                 }
             });
-        } else if (window.location.href.includes('portal.do')) {
-            // login page, /portal.do*
-            getNotice('login');
-            chrome.storage.sync.get(['username', 'password'], function work({ username, password }) {
-                if (username && password) {
-                    let object;
-                    try {
-                        object = await sendLoginRequest(username, password);
-                    } catch (reason) {
-                        alert(chrome.i18n.getMessage('errorOnRequest'), [reason]);
-                        console.error(`Encountered error when sending request: ${reason}`);
-                    }
-                    if (object.code == 0) {
-                        chrome.storage.sync.get(['autoClose'], ({ autoClose }) => {
-                            if (autoClose) {
-                                chrome.runtime.sendMessage({ closeTab: true });
-                            }
-                        });
-                    } else {
-                        const ERROR = chrome.i18n.getMessage('errorOnLogin', object.message);
-                        if (confirm(`${ERROR} ${chrome.i18n.getMessage('tryAgain')}`)) {
-                            work({ username, password });
-                        }
-                    }
-                } else {
-                    const INVALID_INPUT_ALERT =
-                    `${chrome.i18n.getMessage('extInvalidInputAlert')} 
-                    ${username} ${password}`;
-                    alert(INVALID_INPUT_ALERT);
-                    chrome.runtime.openOptionsPage();
-                }
-            });
-        } else {
-            // user manually goes to the logout page, portalLogout.do*.
-            // do nothing but save the notice
-            getNotice('logout');
-        }
-    }
-};
+    } else if (window.location.href.includes('portal.do')) {
+        // login page, /portal.do*
+        getNotice('login');
 
-app.start()
-.catch((error) => {
-    alert(chrome.i18n.getMessage('errorFromExtension', [error]));
-    console.error(`Encountered error when trying to login: ${error}`);
-});
+        let { username, password } = await getValue(['username', 'password']);
+
+        if (username && password) {
+            let object;
+
+            try {
+                object = await sendLoginRequest(username, password);
+            } catch (reason) {
+                alert(chrome.i18n.getMessage('errorOnRequest'), [reason]);
+                console.error(`Encountered error when sending request: ${reason}`);
+            }
+            
+            if (object.code == 0) {
+                getValue(['autoClose'])
+                    .then(({ autoClose }) => {
+                        if (autoClose) {
+                            chrome.runtime.sendMessage({ closeTab: true });
+                        }
+                    });
+            } else {
+                const ERROR = chrome.i18n.getMessage('errorOnLogin', object.message);
+                
+                if (confirm(`${ERROR} ${chrome.i18n.getMessage('tryAgain')}`)) {
+                    await start();
+                }
+            }
+        } else {
+            const INVALID_INPUT_ALERT =
+            `${chrome.i18n.getMessage('extInvalidInputAlert')} 
+            ${username} ${password}`;
+            alert(INVALID_INPUT_ALERT);
+            chrome.runtime.openOptionsPage();
+        }
+    } else {
+        // user manually goes to the logout page, portalLogout.do*.
+        // do nothing but save the notice
+        getNotice('logout');
+    }
+}
+
+start()
+    .catch((error) => {
+        alert(chrome.i18n.getMessage('errorFromExtension', [error]));
+        console.error(`Encountered error when trying to login: ${error}`);
+    });
