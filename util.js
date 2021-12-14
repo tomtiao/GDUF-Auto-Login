f1fef22c73f6d32d0598756ad8c94e56abdf08be
tomtiao <tomtiao1234@gmail.com>
2021-12-14 16:17:21 +0800

    remove getParamsOf since URL.searchParams already done the job

--- util.js
+++ util.js
@@ -4,16 +4,6 @@
 const /** @type {<T>(keys: string[]) => Promise<Record<string, T>>} */ getValue =
     async (keys) => new Promise(resolve => chrome.storage.sync.get(keys, resolve));
 
-/**
- * Return the search parameters of the url
- * @param {string} url 
- * @return {URLSearchParams}
- */
-function getParamsOf(url) {
-    return new URLSearchParams(url);
-}
-
 export {
-    getValue,
-    getParamsOf
+    getValue
 }
